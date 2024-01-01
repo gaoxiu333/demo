cbddc481b0347edbcce8e62f482b18b08e797983
gaoxiu <gaoxiu333@gmail.com>
2024-01-01 12:39:42 +0800

    快速排序

---
+++ 排序/快速排序.js
@@ -0,0 +1,24 @@
+
+function quickSort(originalArray) {
+  const array = [...originalArray];
+  if (array.length <= 1) {
+    return array;
+  }
+  const leftArray = [];
+  const rightArray = [];
+  const pivotElement = array.shift();
+  const centerArray = [pivotElement];
+  while (array.length) {
+    const currentElement = array.shift();
+    if (currentElement === pivotElement) {
+      centerArray.push(currentElement);
+    } else if (currentElement < pivotElement) {
+      leftArray.push(currentElement);
+    } else {
+      rightArray.push(currentElement);
+    }
+  }
+  const leftArraySorted = quickSort(leftArray);
+  const rightArraySorted = quickSort(rightArray);
+  return leftArraySorted.concat(centerArray, rightArraySorted);
+}
