271ab52fa0834549b042f15ac654063f91dcd697
gaoxiu <gaoxiu333@gmail.com>
2024-01-07 20:27:15 +0800

    demo

--- js-example/拷贝.js
+++ js-example/拷贝.js
@@ -79,3 +79,20 @@ function deepClone(target, hash = new WeakMap()) {
   }
   return clone;
 }
+
+////////////////////////////////
+
+const shallowClone = (obj) => Object.assign({}, obj);
+
+const deepClone = (obj) => {
+  if (obj === null) return null;
+  let clone = Object.assign({}, obj);
+  Object.keys(clone).forEach((key) => {
+    clone[key] = typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key];
+  });
+  if (Array.isArray(obj)) {
+    clone.length = obj.length;
+    return Array.from(clone);
+  }
+  return clone;
+};
